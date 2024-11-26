9f1090cb328267e9ba77c5c1b6e89b5da714213b
Mariano Hernandez <mariano.escobal@cencosud.com.uy>
2024-11-25 22:34:53 -0300

    Change plp

--- src/components/atomos/PropertyCard.tsx
+++ src/components/atomos/PropertyCard.tsx
@@ -72,48 +72,53 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
                 rounded-lg
             `}
             >
-                <figure className="w-full relative rounded-lg mb-4 group">
-                    <div className="relative w-full h-full">
-                        {isLoading && (
-                            <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-50">
-                                <div className="loader"></div>
-                            </div>
-                        )}
-                        <img
-                            src={imageSrc[currentImageIndex] ?? image}
-                            alt={title}
-                            className="w-full h-full object-cover rounded-t-lg"
-                            onLoad={handleImageLoad}
-                        />
-                    </div>
-                    <div className="absolute inset-0 flex justify-between items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                        <button
-                            className="bg-black bg-opacity-20 hover:bg-opacity-50 text-white p-2 rounded-full ml-2"
-                            onClick={handlePrevImage}
-                        >
-                            &lt;
-                        </button>
-                        <Button to={`/propiedades/${id}`} clase={"bg-white bg-opacity-30 hover:bg-white hover:text-black hover:bg-opacity-100"}>Ver Propiedad</Button>
-                        <button
-                            className="bg-black bg-opacity-20 hover:bg-opacity-50 text-white p-2 rounded-full mr-2"
-                            onClick={handleNextImage}
-                        >
-                            &gt;
-                        </button>
-                    </div>
-                    <div className="absolute bottom-2 right-0 flex">
-                        <button
-                            className={`text-white text-xl py-2 px-4 rounded transition duration-300  ${selectedButtons.heart
-                                ? "text-red-500"
-                                : "hover:text-red-500 rounded-xl"
-                                }`}
-                            onClick={() => toggleButton("heart")}
-                        >
-                            ♥
-                        </button>
+<figure className="w-full relative rounded-lg mb-4 group">
+    <div className="relative w-full h-full">
+        {isLoading && (
+            <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-50">
+                <div className="loader"></div>
+            </div>
+        )}
+        <img
+            src={imageSrc[currentImageIndex] ?? image}
+            alt={title}
+            className="w-full h-full object-cover rounded-t-lg transition duration-300 group-hover:opacity-80 peer-hover:opacity-100 peer-hover:bg-green-500"
+            onLoad={handleImageLoad}
+        />
+    </div>
+    <div className="absolute inset-0 flex justify-between items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <button
+            className="bg-black bg-opacity-20 hover:bg-opacity-50 text-white p-2 rounded-full ml-2"
+            onClick={handlePrevImage}
+        >
+            &#8592;
+        </button>
+        <Button
+            to={`/propiedades/${id}`}
+            clase="bg-white bg-opacity-30 hover:bg-white hover:text-black hover:bg-opacity-100 peer"
+        >
+            Ver Propiedad
+        </Button>
+        <button
+            className="bg-black bg-opacity-20 hover:bg-opacity-50 text-white p-2 rounded-full mr-2"
+            onClick={handleNextImage}
+        >
+            &#8594;
+        </button>
+    </div>
+    <div className="absolute bottom-2 right-1 flex">
+        <button
+            className={`text-white text-2xl py-1 px-2 rounded-md transition duration-300 border-2 ${selectedButtons.heart
+                ? "text-red-500"
+                : "hover:text-red-500 border-transparent"
+                }`}
+            onClick={() => toggleButton("heart")}
+        >
+            ♥
+        </button>
+    </div>
+</figure>
 
-                    </div>
-                </figure>
                 <Link to={`/propiedades/${id}`} className="w-full">
 
                     <div className="flex-grow flex flex-col p-4 sm:p-0">
